74693878096c58a95b74c88d8234e4bd0ec88f3b
Tim Zimmermann <tim@linux4.de>
2022-08-24 15:40:22 +0200

    msm8998-common: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- msm8998.mk
+++ msm8998.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
