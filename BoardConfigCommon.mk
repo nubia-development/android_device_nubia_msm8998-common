kracenra/android_device_nubia_msm8998-common-oss

msm8998-common: Move to *.prop
Change-Id: I66b834b2df9f652ef3173815270d7aa0a85a9e14

BoardConfigCommon.mk
@@ -151,6 +151,10 @@ TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 
 # RIL

nubia-development/android_device_nubia_msm8998-common

BoardConfigCommon.mk
@@ -133,6 +133,13 @@ TARGET_PER_MGR_ENABLED := true
 BOARD_POWER_CUSTOM_BOARD_LIB := libpower_msm8998
 TARGET_USES_INTERACTION_BOOST := true
 
+# Properties
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+TARGET_ODM_PROP += $(PLATFORM_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(PLATFORM_PATH)/product.prop
+TARGET_SYSTEM_PROP += $(PLATFORM_PATH)/system.prop
+TARGET_VENDOR_PROP += $(PLATFORM_PATH)/vendor.prop
+
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
 
@@ -159,7 +166,6 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
 
 # Treble
-BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 PRODUCT_VENDOR_MOVE_ENABLED := true
 