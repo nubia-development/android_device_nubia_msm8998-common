41cde2b9693b3ff3b2df06a676418f3d00da97fe
Cyborg2017 <wangyang0987654@gmail.com>
2023-03-31 23:20:45 +0800

    msm8998-common: Decommonize gatekeeper and keymaster
    * nx609j only boot with prebuilt gatekeeper and keymaster hals
    
    Change-Id: I6ff29c343f2ef3b9ddde4d6cfc90623f013bb11d

--- msm8998.mk
+++ msm8998.mk
@@ -174,11 +174,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
 
-# Gatekeeper
-PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service
-
 # GPS
 PRODUCT_PACKAGES += \
     libgnss
@@ -226,11 +221,6 @@ PRODUCT_COPY_FILES += \
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
-
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
